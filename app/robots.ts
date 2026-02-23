1d4081265f19fd0cd5177e973ba1672d90aaf5a7
Confidencedev837 <confidenceorok30@gmail.com>
2026-02-23 18:52:28 +0100

    sb

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/admin/',
         },
-        sitemap: 'https://gracelogisticsfoodanditems.vercel.app/sitemap.xml',
+        sitemap: 'https://gracelogisticsfoodanditems.vercel.app/sitema.xml',
     };
 }
